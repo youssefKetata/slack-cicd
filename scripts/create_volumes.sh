b4bebff971eda92c116f4fc986ad82e80d7bad14
EdamH <edam.hamza@supcom.tn>
2024-06-05 00:49:38 +0100

    update: added global readme file

--- scripts/create_volumes.sh
+++ scripts/create_volumes.sh
@@ -1,1 +1,10 @@
-docker volume create ops_jenkins ops_prometheus ops_grafana env-dev_db_staging prod_db_prod
+# Create volumes for Jenkins, Prometheus and Grafana
+sudo mkdir -p ../ops/var/prometheus/prometheus_data
+sudo mkdir -p ../ops/etc/prometheus
+sudo chmod -R 777 ../ops/var/prometheus/prometheus_data
+sudo chmod -R 777 ../ops/etc/prometheus
+
+sudo mkdir -p ../ops/var/grafana/grafana_data
+sudo mkdir -p ../ops/etc/grafana/provisioning
+sudo chmod -R 777 ../ops/var/grafana/grafana_data
+sudo chmod -R 777 ../ops/etc/grafana/provisioning
